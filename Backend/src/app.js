e9f6d2fadd0705f929359e9429758ffb5cca259e
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-07 13:21:57 +0500

    Update code!

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,11 @@ const messageRoutes = require('./router/message.routes.js')
 const cors = require("cors");
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://portfolio-web-chi-rouge-33.vercel.app' ],
+    origin: [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://portfolio-web-chi-rouge-33.vercel.app"
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']

2d699e43b774b346e7ddd27e4c0b0c45a578c514
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-07 14:18:29 +0500

    Update code!

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://portfolio-jrqazsqjt-mohsin9.vercel.app"
+        "https://portfolio-5l1wymphr-mohsin9.vercel.app"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],

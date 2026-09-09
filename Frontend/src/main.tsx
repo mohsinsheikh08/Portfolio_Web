f9eb981066b68631c52bcb2b019ea7e1b3e4314c
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-09 18:32:42 +0500

    Update code!

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <BrowserRouter>
-      <App />
+       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>,
 );
